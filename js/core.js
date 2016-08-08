38def64003d95f4ecea94da909bf06065a99d58d
mayo589 <marekmihalech@gmail.com>
2016-08-08 21:56:50 +0200

    added line chart with sports activity duration by month

--- js/core.js
+++ js/core.js
@@ -68,7 +68,7 @@ $(document).ready(function () {
 
         $("#not-logged").hide();
         $("#logged-in").show();
-       
+        displayCharts();
     }
     else{
         $("#not-logged").show();
@@ -149,7 +149,7 @@ function parseAndDisplay(buckets){
                 for(var j = 0; j < bucket.activities.length; j++){
                     var activity = bucket.activities[j];
                     bucketSpan += "<br><span style='margin-left: 50px;'></span>";
-                    bucketSpan += " Activity type: " + activity.type; 
+                    bucketSpan += " Activity type: " + activity.type + " - " + activity.name; 
                     bucketSpan += " Start: " + activity.startDate.toString();
                     bucketSpan += " End: " + activity.endDate.toString();
                     bucketSpan += " Duration: " + activity.durationMillis;
@@ -161,4 +161,344 @@ function parseAndDisplay(buckets){
             $(resultContainser).append(bucketSpan);
         }
     }
+}
+
+function displayCharts(){
+    var ctxCals = document.getElementById("chart-calories");
+    var chartCals = new Chart(ctxCals, 
+    {
+        type: 'bar',
+        data: {
+            labels: getLastNMonthNames(6, 1),
+            datasets: [{
+                label: 'Total Calories per month',
+                data: getLastNMonthsCalories(6, 1),
+                borderWidth: 1,
+                backgroundColor: "rgba(75,192,192,0.6)",
+                borderColor: "rgba(75,192,192,1)",
+                borderJoinStyle: 'miter',
+                pointHitRadius: 10,
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                            callback: function(value, index, values) {
+                                return Math.round(value / 1000) + "k"
+                            },
+                    }
+                }]
+            },
+            tooltips: {
+                mode: 'label',
+                callbacks: {
+                    title: function(item, data) {
+                        var title = "";
+                        title += "Total Calorie outtake in " + item[0].xLabel + ": " + item[0].yLabel;
+                        return title;
+                    },
+                    label: function(item, data){
+                        var afterTitle = "";
+                        var month = getMonthByName(item.xLabel)
+                        afterTitle += "Average per day: " + Math.round(parseInt(item.yLabel) /  daysInMonth(month, (new Date()).getFullYear())); 
+                        return afterTitle;
+                    },
+                    
+                }
+            },
+            responsive: false
+        }, 
+    });
+    
+    
+    var ctxDist = document.getElementById("chart-distance");
+    var chartDistance = new Chart(ctxDist, 
+    {
+        type: 'line',
+        data: {
+            labels: getLastNMonthNames(6, 1),
+            datasets: [{
+                label: 'Total Distance per month',
+                data: getLastNMonthsDistance(6, 1),
+                borderWidth: 1,
+                backgroundColor: "rgba(63,191,63,0.6)",
+                borderColor: "rgba(63,191,63,1)",
+                borderJoinStyle: 'miter',
+                pointHitRadius: 10,
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        callback: function(value, index, values) {
+                                return Math.round(value) + " km"
+                            },
+                    }
+                }]
+            },
+            tooltips: {
+                mode: 'label',
+                callbacks: {
+                    title: function(item, data) {
+                        var title = "";
+                        title += "Total distance in " + item[0].xLabel + ": " + parseInt(item[0].yLabel)  + " km";
+                        return title;
+                    },
+                    label: function(item, data){
+                        var afterTitle = "";
+                        var month = getMonthByName(item.xLabel)
+                        afterTitle += "Average per day: " + ( (parseInt(item.yLabel) /  daysInMonth(month, (new Date()).getFullYear())) ).toFixed(2) + " km"; 
+                        return afterTitle;
+                    },
+                    
+                }
+            },
+            responsive: false
+        }, 
+    });
+    
+    var ctxSteps = document.getElementById("chart-steps");
+    var chartSteps = new Chart(ctxSteps, 
+    {
+        type: 'bar',
+        data: {
+            labels: getLastNMonthNames(6, 1),
+            datasets: [{
+                label: 'Total step count per month',
+                data: getLastNMonthsSteps(6, 1),
+                borderWidth: 1,
+                backgroundColor: "rgba(191,63,63,0.6)",
+                borderColor: "rgba(191,63,63,1)",
+                borderJoinStyle: 'miter',
+                pointHitRadius: 10,
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                            callback: function(value, index, values) {
+                                return Math.round(value / 1000) + "k"
+                            },
+                    }
+                }]
+            },
+            tooltips: {
+                mode: 'label',
+                callbacks: {
+                    title: function(item, data) {
+                        var title = "";
+                        title += "Total step count in " + item[0].xLabel + ": " + parseInt(item[0].yLabel);
+                        return title;
+                    },
+                    label: function(item, data){
+                        var afterTitle = "";
+                        var month = getMonthByName(item.xLabel)
+                        afterTitle += "Average per day: " + Math.round( (parseInt(item.yLabel) /  daysInMonth(month, (new Date()).getFullYear())) ); 
+                        return afterTitle;
+                    },
+                    
+                }
+            },
+            responsive: false
+        }, 
+    });
+    
+    var datasetActivities = getLastNMonthsActivities(6, 1);
+    var ctxActivities = document.getElementById("chart-activities");
+    var chartActivities = new Chart(ctxActivities, 
+    {
+        type: 'line',
+        data: {
+            labels: getLastNMonthNames(6, 1),
+            datasets: datasetActivities, 
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                            callback: function(value, index, values) {
+                                return msToTime(value, "short");
+                            },
+                            stepSize: 10800000
+                    },
+                }]
+            },
+            tooltips: {
+                    mode: 'label', //single or label 
+                    callbacks: {
+                        title: function(item, data) {
+                            var title = "";
+                            var totalDuration = 0;
+                            for(var i = 0; i < item.length; i++){
+                                totalDuration += item[i].yLabel;
+                            }
+                            title += "Total sport activity duration in " + item[0].xLabel + ": " + msToTime(totalDuration);
+                            return title;
+                        },
+                        label: function(item, data){
+                            var afterTitle = "";
+                            var month = getMonthByName(item.xLabel);
+                            afterTitle += data.datasets[item.datasetIndex].label;
+                            afterTitle += " total: " + msToTime(item.yLabel, "short");
+                            afterTitle += " Average per day: " +  msToTime( (item.yLabel /  daysInMonth(month, (new Date()).getFullYear())), "short" ); 
+                            return afterTitle;
+                        },
+                    }
+                },
+            responsive: false
+        }, 
+    });
+}
+
+
+
+
+
+
+var monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+
+function daysInMonth(month,year) {
+    return new Date(year, month, 0).getDate();
+}
+
+function getMonthByName(monthName){
+    for(var i = 0; i < monthNames.length; i++){
+        if(monthNames[i] == monthName)
+            return i;
+    }
+}
+
+
+function getLastNMonthNames(numOfMonth, offset){
+    var retMonthNames = [];
+    for(var i = 0; i < numOfMonth; i++){
+        var d = new Date();
+        d.setMonth(d.getMonth()-i-offset);
+        retMonthNames.push(monthNames[d.getMonth()]);
+    }
+    return retMonthNames.reverse();
+}
+
+
+
+function getLastNMonthsCalories(numOfMonth, offset){
+    var monthCals = [];
+    for(var i = 0; i < numOfMonth; i++){
+        var today = new Date();
+        var startDate = new Date(today.getFullYear(), today.getMonth()-i-offset, 1, 0, 0, 0, 0)
+        var endDate = new Date(today.getFullYear(), today.getMonth()-i-offset+1, 0, 0, 0, 0, 0)
+        
+        var result = getAggregatedData(DataTypeName.CALORIES, startDate, endDate, BucketTimeMillis.MONTH )
+        monthCals.push(Math.round(result[0].value));
+    }
+    return monthCals.reverse();
+}
+
+function getLastNMonthsSteps(numOfMonth, offset){
+    var monthCals = [];
+    for(var i = 0; i < numOfMonth; i++){
+        var today = new Date();
+        var startDate = new Date(today.getFullYear(), today.getMonth()-i-offset, 1, 0, 0, 0, 0)
+        var endDate = new Date(today.getFullYear(), today.getMonth()-i-offset+1, 0, 0, 0, 0, 0)
+        
+        var result = getAggregatedData(DataTypeName.STEPS, startDate, endDate, BucketTimeMillis.MONTH )
+        monthCals.push(Math.round(result[0].value));
+    }
+    return monthCals.reverse();
+}
+
+function getLastNMonthsDistance(numOfMonth, offset){
+    var monthDist = [];
+    for(var i = 0; i < numOfMonth; i++){
+        var today = new Date();
+        var startDate = new Date(today.getFullYear(), today.getMonth()-i-offset, 1, 0, 0, 0, 0)
+        var endDate = new Date(today.getFullYear(), today.getMonth()-i-offset+1, 0, 0, 0, 0, 0)
+        
+        var result = getAggregatedData(DataTypeName.DISTANCE, startDate, endDate, BucketTimeMillis.MONTH )
+        monthDist.push(Math.round(result[0].value) / 1000);
+    }
+    return monthDist.reverse();
+}
+
+function getLastNMonthsActivities(numOfMonth, offset){
+    var monthActivities = [];
+    
+    for(var i = 0; i < numOfMonth; i++){
+        var today = new Date();
+        var startDate = new Date(today.getFullYear(), today.getMonth()-i-offset, 1, 0, 0, 0, 0)
+        var endDate = new Date(today.getFullYear(), today.getMonth()-i-offset+1, 0, 0, 0, 0, 0)
+        
+        var result = getAggregatedData(DataTypeName.ACTIVITIES, startDate, endDate, BucketTimeMillis.MONTH )
+        
+        
+        for(var j = 0; j < result[0].activities.length; j++){
+            var activity = result[0].activities[j];
+            if(activity.name.match(/still/i) ||
+               activity.name.match(/sleep/i) ||
+               activity.name.match(/vehicle/i) ||
+               activity.name.match(/walking/i) ){
+                   continue;
+               }
+            
+            if(typeof monthActivities[activity.type] == "undefined"){
+                var color = randomColor({
+                    luminosity: 'light',
+                    format: 'rgba',
+                    alpha: 0.6 
+                });
+                monthActivities[activity.type] = {
+                    label: activity.name,
+                 
+                    data: Array.apply(null, Array(numOfMonth)).map(function() { return 0 }),
+                    borderWidth: 1,
+                    backgroundColor: color,
+                    borderColor: color.replace("0.6", "1"),
+                    borderJoinStyle: 'miter',
+                    pointHitRadius: 10,       
+                };
+                
+            }
+            
+            monthActivities[activity.type].data[numOfMonth - i - 1] = activity.durationMillis;
+        }        
+    }
+    return monthActivities.filter(function(val){return val});
+}
+
+function msToTime(s, type) {
+    var strSecs, strHours, strMins = "";
+    if(type == "short"){
+        strHours = "h ";
+        strMins = "m ";
+        strSecs = "s ";
+    }
+    else{
+        strHours = " hours ";
+        strMins = " minutes ";
+        strSecs = " seconds ";
+    }
+    
+  var ms = s % 1000;
+  s = (s - ms) / 1000;
+  var secs = s % 60;
+  s = (s - secs) / 60;
+  var mins = s % 60;
+  var hrs = (s - mins) / 60;
+
+  var retVal = "";
+  if(hrs > 0){
+        retVal += hrs + strHours; 
+  }
+  if(mins > 0){
+      retVal += mins + strMins;
+  }
+  if(secs > 0 || retVal == ""){
+      retVal += secs + strSecs;
+  }
+  return retVal;
 }
--- js/gfitRest.js
+++ js/gfitRest.js
@@ -51,10 +51,20 @@ function getAggregatedData(dataTypeName, startDate, endDate, bucketMillis) {
                 switch(dataTypeName) {
                 case DataTypeName.CALORIES:
                 case DataTypeName.DISTANCE:
-                    bucketParsed.value = dataBucket.dataset[0].point[0].value[0].fpVal;
+                    if(typeof dataBucket.dataset[0].point != "undefined"){
+                        bucketParsed.value = dataBucket.dataset[0].point[0].value[0].fpVal;
+                    }
+                    else{
+                        bucketParsed.value = 0;
+                    }
                     break;
                 case  DataTypeName.STEPS:
-                    bucketParsed.value = dataBucket.dataset[0].point[0].value[0].intVal; 
+                    if(typeof dataBucket.dataset[0].point != "undefined"){
+                        bucketParsed.value = dataBucket.dataset[0].point[0].value[0].intVal; 
+                    }
+                    else{
+                        bucketParsed.value = 0;
+                    }
                     break;
                 case DataTypeName.ACTIVITIES:
                 {
@@ -63,6 +73,7 @@ function getAggregatedData(dataTypeName, startDate, endDate, bucketMillis) {
                         var activityData = dataBucket.dataset[0].point[j];
                         var activityParsed = {};
                         activityParsed.type = activityData.value[0].intVal;
+                        activityParsed.name = activityTypes[activityParsed.type]; 
                         activityParsed.durationMillis = activityData.value[1].intVal;
                         activityParsed.startDate =  (new Date( parseInt(activityData.startTimeNanos.substr(0, activityData.startTimeNanos.length-6) )));  
                         activityParsed.endDate =  (new Date( parseInt(activityData.endTimeNanos.substr(0, activityData.endTimeNanos.length-6) )));
@@ -84,3 +95,124 @@ function getAggregatedData(dataTypeName, startDate, endDate, bucketMillis) {
     });
     return retVal;
 }
+
+function getActivityTypeIndex(name){
+    for(var i = 0; i < activityTypes.length; i++){
+        if(name == activityTypes[i])
+            return i;
+    }
+}
+
+var activityTypes = [];
+activityTypes[0]= 'In vehicle';
+activityTypes[1]= 'Biking';
+activityTypes[2]= 'On foot';
+activityTypes[3]= 'Still (not moving)';
+activityTypes[4]= 'Unknown (unable to detect activity)';
+activityTypes[5]= 'Tilting (sudden device gravity change)';
+activityTypes[7]= 'Walking';
+activityTypes[8]= 'Running';
+activityTypes[9]= 'Aerobics';
+activityTypes[10]= 'Badminton';
+activityTypes[11]= 'Baseball';
+activityTypes[12]= 'Basketball';
+activityTypes[13]= 'Biathlon';
+activityTypes[14]= 'Handbiking';
+activityTypes[15]= 'Mountain biking';
+activityTypes[16]= 'Road biking';
+activityTypes[17]= 'Spinning';
+activityTypes[18]= 'Stationary biking';
+activityTypes[19]= 'Utility biking';
+activityTypes[20]= 'Boxing';
+activityTypes[21]= 'Calisthenics';
+activityTypes[22]= 'Circuit training';
+activityTypes[23]= 'Cricket';
+activityTypes[24]= 'Dancing';
+activityTypes[25]= 'Elliptical';
+activityTypes[26]= 'Fencing';
+activityTypes[27]= 'Football (American)';
+activityTypes[28]= 'Football (Australian)';
+activityTypes[29]= 'Football (Soccer)';
+activityTypes[30]= 'Frisbee';
+activityTypes[31]= 'Gardening';
+activityTypes[32]= 'Golf';
+activityTypes[33]= 'Gymnastics';
+activityTypes[34]= 'Handball';
+activityTypes[35]= 'Hiking';
+activityTypes[36]= 'Hockey';
+activityTypes[37]= 'Horseback riding';
+activityTypes[38]= 'Housework';
+activityTypes[39]= 'Jumping rope';
+activityTypes[40]= 'Kayaking';
+activityTypes[41]= 'Kettlebell training';
+activityTypes[42]= 'Kickboxing';
+activityTypes[43]= 'Kitesurfing';
+activityTypes[44]= 'Martial arts';
+activityTypes[45]= 'Meditation';
+activityTypes[46]= 'Mixed martial arts';
+activityTypes[47]= 'P90X exercises';
+activityTypes[48]= 'Paragliding';
+activityTypes[49]= 'Pilates';
+activityTypes[50]= 'Polo';
+activityTypes[51]= 'Racquetball';
+activityTypes[52]= 'Rock climbing';
+activityTypes[53]= 'Rowing';
+activityTypes[54]= 'Rowing machine';
+activityTypes[55]= 'Rugby';
+activityTypes[56]= 'Jogging';
+activityTypes[57]= 'Running on sand';
+activityTypes[58]= 'Running (treadmill)';
+activityTypes[59]= 'Sailing';
+activityTypes[60]= 'Scuba diving';
+activityTypes[61]= 'Skateboarding';
+activityTypes[62]= 'Skating';
+activityTypes[63]= 'Cross skating';
+activityTypes[64]= 'Inline skating (rollerblading)';
+activityTypes[65]= 'Skiing';
+activityTypes[66]= 'Back-country skiing';
+activityTypes[67]= 'Cross-country skiing';
+activityTypes[68]= 'Downhill skiing';
+activityTypes[69]= 'Kite skiing';
+activityTypes[70]= 'Roller skiing';
+activityTypes[71]= 'Sledding';
+activityTypes[72]= 'Sleeping';
+activityTypes[73]= 'Snowboarding';
+activityTypes[74]= 'Snowmobile';
+activityTypes[75]= 'Snowshoeing';
+activityTypes[76]= 'Squash';
+activityTypes[77]= 'Stair climbing';
+activityTypes[78]= 'Stair-climbing machine';
+activityTypes[79]= 'Stand-up paddleboarding';
+activityTypes[80]= 'Strength training';
+activityTypes[81]= 'Surfing';
+activityTypes[82]= 'Swimming';
+activityTypes[83]= 'Swimming (swimming pool)';
+activityTypes[84]= 'Swimming (open water)';
+activityTypes[85]= 'Table tennis (ping pong)';
+activityTypes[86]= 'Team sports';
+activityTypes[87]= 'Tennis';
+activityTypes[88]= 'Treadmill (walking or running)';
+activityTypes[89]= 'Volleyball';
+activityTypes[90]= 'Volleyball (beach)';
+activityTypes[91]= 'Volleyball (indoor)';
+activityTypes[92]= 'Wakeboarding';
+activityTypes[93]= 'Walking (fitness)';
+activityTypes[94]= 'Nording walking';
+activityTypes[95]= 'Walking (treadmill)';
+activityTypes[96]= 'Waterpolo';
+activityTypes[97]= 'Weightlifting';
+activityTypes[98]= 'Wheelchair';
+activityTypes[99]= 'Windsurfing';
+activityTypes[100]= 'Yoga';
+activityTypes[101]= 'Zumba';
+activityTypes[102]= 'Diving';
+activityTypes[103]= 'Ergometer';
+activityTypes[104]= 'Ice skating';
+activityTypes[105]= 'Indoor skating';
+activityTypes[106]= 'Curling';
+activityTypes[108]= 'Other (unclassified fitness activity)';
+activityTypes[109]= 'Light sleep';
+activityTypes[110]= 'Deep sleep';
+activityTypes[111]= 'REM sleep';
+activityTypes[112]= 'Awake (during sleep cycle)';
+
